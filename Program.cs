7fa97c6e8ac592f9ee14ff5ae8947b685155463c
fermar7 <fermar0715@gmail.com>
2017-02-08 20:40:47 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validator {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+
+            var v = new CommonValidatorBuilder<string>()
+                                    .AddRule(s => s.Length < 10)
+                                    .AddRule(s => s.Length > 0)
+                                    .Build();
+            
+
+        }
+    }
+
+    public class CommonValidator<TSource> {
+
+        private IList<Predicate<TSource>> rules;
+
+        internal CommonValidator(IList<Predicate<TSource>> predicates) {
+            this.rules = predicates;
+        } 
+        
+    }
+
+    public class CommonValidatorBuilder<TSource> {
+
+        private IList<Predicate<TSource>> predicates;
+
+        public CommonValidatorBuilder() {
+            this.predicates = new List<Predicate<TSource>>();
+        }
+
+        public CommonValidatorBuilder<TSource> AddRule(Predicate<TSource> predicate) {
+            this.predicates.Add(predicate);
+            return this;
+        }
+
+        public CommonValidator<TSource> Build() {
+            return new CommonValidator<TSource>(predicates);
+        }
+
+    }
+
+    public abstract class BaseRule<TSource> {
+
+    }
+
+    public class SimpleRule<TSource> : BaseRule<TSource> {
+
+    }
+
+    public class DatabaseRule<TSource> : BaseRule<TSource> {
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("Validator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Validator")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("63fa8f9a-e2b6-4d5e-873d-4bf92c2b9c1e")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
